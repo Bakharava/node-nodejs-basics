924fb6b07efb1afb50a4d612c87bbf490f5bf930
Volha_Bakharava <olavlad777@gmail.com>
2022-12-03 21:46:07 +0400

    HW-1 Node.js basics. Hash module

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import crypto from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const pathToFile = path.join('src', 'hash', 'files', 'fileToCalculateHashFor.txt');
+  await fs.readFile(pathToFile, (err, data) => {
+    if (err) throw 'eeeERrr' + err;
+    createHexHesh(data)
+  });
+
+  const createHexHesh = (data) => {
+    const hash = crypto.createHash('sha256');
+    hash.update(data)
+
+    const hexHash = hash.digest('hex');
+    console.log(hexHash);
+  }
 };
 
 await calculateHash();
