17b5db6da0459f3ba5e67e3742086c6107b7cbb4
Volha_Bakharava <olavlad777@gmail.com>
2022-12-04 21:57:57 +0400

    HW-1 Node.js basics. Streams module

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import process from 'process';
+
 const read = async () => {
-    // Write your code here 
+  const pathToFile = path.join('src', 'streams', 'files', 'fileToRead.txt');
+  const stream = fs.createReadStream(pathToFile, 'utf-8');
+  let data = '';
+
+  stream.on('error', (err) => {
+    throw new Error(`Cannot read file ${pathToFile}. Error: ${err.message}`)
+  });
+
+  stream.on('data', (chunk) => {
+    data += chunk
+  })
+
+  stream.on('end', () =>  process.stdout.write(data));
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import process from 'process';
+
 const write = async () => {
-    // Write your code here 
+  const pathToFile = path.join('src', 'streams', 'files', 'fileToWrite.txt');
+    let input = process.stdin;
+    const output = fs.createWriteStream(pathToFile);
+  input.on('data', (data) => output.write(data));
+  input.on('error', error => console.log('Error', error.message));
 };
 
 await write();
