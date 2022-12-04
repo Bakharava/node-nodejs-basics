fe6ad10dd6ec7586c06e0338f888d041b04f874f
Volha_Bakharava <olavlad777@gmail.com>
2022-12-04 21:15:52 +0400

    HW-1 Node.js basics. CLI module

--- src/cli/args.js
+++ src/cli/args.js
@@ -2,11 +2,16 @@ import process from 'process';
 
 const parseArgs = () => {
   const prefix = '--'
+  const argList = [];
   process.argv.length && process.argv.forEach((arg, index) => {
+
     if (arg.includes(prefix)) {
-      console.log(`${arg} is ${process.argv[index + 1]}`)
+      const argParam = arg.substr(2);
+      argList.push(`${argParam} is ${process.argv[index + 1]}`)
     }
   })
+  const output = argList.join(', ')
+  console.log(output)
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,12 +1,18 @@
 import process from 'process';
 
 const parseEnv = () => {
-  const prefix = 'RSS_'
-  process.argv.length && process.argv.forEach((arg) => {
-      if (arg.includes(prefix)) {
-        console.log(arg)
+  const prefix = 'RSS_';
+  let envVarList = [];
+
+  if (Object.keys(process.env).length) {
+    for (let key in process.env) {
+      if (key.includes(prefix)) {
+        envVarList.push(`${key} is ${process.env[key]}`)
       }
-    })
+    }
+    const output = envVarList.join('; ');
+    console.log(output)
+  }
 };
 
 parseEnv();
