5646872a5adbade30bac68ec41cf105b19582f7b
Volha_Bakharava <olavlad777@gmail.com>
2022-12-05 22:21:13 +0400

    HW-1 Node.js basics. Cp module

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,18 @@
+import process from 'process';
+import child_process from 'child_process';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+let input = process.stdin;
 const spawnChildProcess = async (args) => {
-    // Write your code here
+
+  const chiled = child_process.spawn('node', [join(__dirname, 'files', 'script.js'), args])
+  chiled.stdout.pipe(process.stdout)
+  input.on('data', (data) => {
+    chiled.stdin.write(data)
+  });
 };
 
-spawnChildProcess();
+spawnChildProcess(process.argv);
