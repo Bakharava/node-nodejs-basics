5cddc406cd175a8196d38aff4d855912cc8c9ebc
Volha_Bakharava <olavlad777@gmail.com>
2022-12-05 10:36:31 +0400

    HW-1 Node.js basics. Zlib module

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,15 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import zlib from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+  const pathToOriginFile = path.join( 'files', 'fileToCompress.txt');
+  const pathToArchivedFile = path.join( 'files', 'archive.gz');
+  const input = fs.createReadStream(pathToOriginFile, 'utf-8');
+  const output = fs.createWriteStream(pathToArchivedFile);
+  const gzip = zlib.createGzip();
+
+  input.pipe(gzip).pipe(output);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,15 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import zlib from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+  const pathToOriginFile = path.resolve( 'files', 'fileToCompress.txt');
+  const pathArchivedFile = path.resolve( 'files', 'archive.gz');
+  const input = fs.createReadStream(pathArchivedFile);
+  const output = fs.createWriteStream(pathToOriginFile);
+  const gzip = zlib.createUnzip();
+
+  input.pipe(gzip).pipe(output);
 };
 
 await decompress();
